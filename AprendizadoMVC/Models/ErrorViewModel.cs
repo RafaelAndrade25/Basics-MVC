97ee6124cf7d587e7105b382f319cc5953ca9f42
Rafael Andrade Bezerra de Araujo <rafael.andrade@chb.local>
2024-05-17 09:36:56 -0300

    17-05-2024

---
+++ AprendizadoMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AprendizadoMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ AprendizadoMVC/Models/HomeModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AprendizadoMVC.Models
+{
+    public class HomeModel
+    {
+        public string Nome { get; set;}
+        public string Email { get; set;}
+
+    }
+}
